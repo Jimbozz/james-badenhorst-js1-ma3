b33eb4c7772b9c94634738fc67b18915f6b6e4ba
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-08-28 16:04:33 +0200

    Deleted spacing

--- js/question-2.js
+++ js/question-2.js
@@ -7,8 +7,6 @@ const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const headers = new Headers()
 const resultsContainer = document.querySelector(".results");
 
-
-
 fetch(url)
 .then(res =>{
     return res.json()
@@ -18,13 +16,11 @@ fetch(url)
     resultsContainer.innerHTML = "";
 
     for (let i = 0; i < 8; i++) {
-        console.log(data.results[i].name);
-
 
         resultsContainer.innerHTML += cardContainer(data.results[i].name, data.results[i].rating, data.results[i].tags);
+
     }
 })
-
 
 .catch(error => { 
     resultsContainer.innerHTML = errorMessage("An Error has occured.", error);
